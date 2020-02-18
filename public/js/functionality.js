efdb388c192385acc8123616252b38b25d1a13af
Sneha Lakshmanan <sneha@ymail.com>
2020-02-18 12:34:22 -0800

    json

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -91,7 +91,6 @@ function initializePage() {
   			//console.log(mins);
 
   			if(mins > 500){
-<<<<<<< HEAD
 				  userTime = 1*60;
 				  text_time = "1 min";
   				$('#min-indicator').text('1min');
@@ -146,104 +145,126 @@ function initializePage() {
 				  text_time = "120 min";
   				$('#min-indicator').text('120min');
   			}
-=======
-  				userTime = 1*60;
+				  (userTime = 1*60);
+				  text_time = "5 min";
   				$('#min-indicator').text('5min');
-  			}
+		
   			if(mins < 500 && mins > 480){
-  				userTime = 5*60;
+				  userTime = 5*60;
+				  text_time = "10 min";
   				$('#min-indicator').text('10min');
   			}
   			if(mins < 480 && mins > 460){
-  				userTime = 10*60;
+				  userTime = 10*60;
+				  text_time = "15 min";
   				$('#min-indicator').text('15min');
   			}
   			if(mins < 460 && mins > 440){
-  				userTime = 20*60;
+				  userTime = 20*60;
+				  text_time = "20 min";
   				$('#min-indicator').text('20min');
   			}
   			if(mins < 440 && mins > 420){
-  				userTime = 25*60;
+				  userTime = 25*60;
+				  text_time = "25 min";
   				$('#min-indicator').text('25min');
   			}
   			if(mins < 420 && mins > 400){
-  				userTime = 30*60;
+				  userTime = 30*60;
+				  text_time = "30 min";
   				$('#min-indicator').text('30min');
   			}
   			if(mins < 400 && mins > 380){
-  				userTime = 35*60;
+				  userTime = 35*60;
+				  text_time = "35 min";
   				$('#min-indicator').text('35min');
   			}
   			if(mins < 380 && mins > 360){
-  				userTime = 40*60;
+				  userTime = 40*60;
+				  text_time = "40 min";
   				$('#min-indicator').text('40min');
   			}
   			if(mins < 360 && mins > 340){
-  				userTime = 45*60;
+				  userTime = 45*60;
+				  text_time = "45 min";
   				$('#min-indicator').text('45min');
   			}
   			if(mins < 340 && mins > 320){
-  				userTime = 50*60;
+				  userTime = 50*60;
+				  text_time = "50 min";
   				$('#min-indicator').text('50min');
   			}
         if(mins < 320 && mins > 300){
-          userTime = 55*60;
+		  userTime = 55*60;
+		  text_time = "55 min";
           $('#min-indicator').text('55min');
         }
         if(mins < 300 && mins > 280){
-          userTime = 60*60;
+		  userTime = 60*60;
+		  text_time = "60 min";
           $('#min-indicator').text('60min');
         }
         /////////// Half way ////////////////
         if(mins < 280 && mins > 260){
-          userTime = 65*60;
+		  userTime = 65*60;
+		  text_time = "65 min";
           $('#min-indicator').text('65min');
         }
         if(mins < 260 && mins > 240){
-          userTime = 70*60;
+		  userTime = 70*60;
+		  text_time = "70 min";
           $('#min-indicator').text('70min');
         }
         if(mins < 240 && mins > 220){
-          userTime = 75*60;
+		  userTime = 75*60;
+		  text_time = "75 min";
           $('#min-indicator').text('75min');
         }
         if(mins < 220 && mins > 200){
-          userTime = 80*60;
+		  userTime = 80*60;
+		  text_time = "80 min";
           $('#min-indicator').text('80min');
         }
         if(mins < 200 && mins > 180){
-          userTime = 85*60;
+		  userTime = 85*60;
+		  text_time = "85 min";
           $('#min-indicator').text('85min');
         }
         if(mins < 180 && mins > 160){
-          userTime = 90*60;
+		  userTime = 90*60;
+		  text_time = "90 min";
           $('#min-indicator').text('90min');
         }
         if(mins < 160 && mins > 140){
-          userTime = 95*60;
+		  userTime = 95*60;
+		  text_time = "95 min";
           $('#min-indicator').text('95min');
         }
         if(mins < 140 && mins > 120){
-          userTime = 100*60;
+		  userTime = 100*60;
+		  text_time = "100 min";
           $('#min-indicator').text('100min');
         }
         if(mins < 120 && mins > 100){
-          userTime = 105*60;
+		  userTime = 105*60;
+		  text_time = "105 min";
           $('#min-indicator').text('105min');
         }
         if(mins < 100 && mins > 80){
-          userTime = 110*60;
+		  userTime = 110*60;
+		  text_time = "110 min";
           $('#min-indicator').text('110min');
         }
         if(mins < 80 && mins > 60){
-          userTime = 115*60;
+		  userTime = 115*60;
+		  text_time = "115 min";
           $('#min-indicator').text('115min');
         }
         if(mins < 60){
-          userTime = 120*60;
+		  userTime = 120*60;
+		  text_time = "120 min";
           $('#min-indicator').text('120min');
         }
->>>>>>> origin/master
 
   		});
 
@@ -356,13 +377,14 @@ function startTimer(duration, display) {
 		  //push plant grown info into json/mygarden page
 		  //var text_time, 
 
-			var fs = require('fs');
+			var fs = require('../plantData.json');
+			console.log("hello");
 
 			fs.readFile('../plantData.json', 'utf-8', function(err, data) {
 				if (err) throw err
 
 				var data = JSON.parse(data);
-				data.users.push({
+				data.plantData.push({
 					name : "filler",
 					image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1PC9OA_1iN5uVi4zFsSNIZeH3Q-qERHI5Pg_nUzMZpe1rfBi4",
 					date : "filler",
@@ -371,7 +393,7 @@ function startTimer(duration, display) {
 
 				console.log(data);
 
-				fs.writeFile('../plantDatajson', JSON.stringify(data), 'utf-8', function(err) {
+				fs.writeFile('../plantData.json', JSON.stringify(data), 'utf-8', function(err) {
 					if (err) throw err
 					console.log('Done!');
 				});
